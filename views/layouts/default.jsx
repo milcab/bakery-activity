0f5231e4ac3a6e92d74a27d83acbd190714a2b83
milcab <milcarodriguez23@gmail.com>
2021-12-21 11:13:26 -0500

    Adding default view for layout

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -1,19 +1,22 @@
-const React = require('react')
+const React = require("react");
 
 function Default(html) {
-    return(
-        <html>
-            <head>
-                <title>Default</title>
-            </head>
-            <body>
-                <h1>HTML Rendered!</h1>
-                <div className="container">
-                    {html.children}
-                </div>
-            </body>
-        </html>
-    )
+  return (
+    <html>
+      <head>
+        <title>{html.title || "default"}</title>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous"
+        />
+      </head>
+      <body>
+        <div className="container">{html.children}</div>
+      </body>
+    </html>
+  );
 }
 
-module.exports = Default
+module.exports = Default;
