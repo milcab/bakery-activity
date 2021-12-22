fd6a5d5ac712639ed67b1f5c1aa72b43314a191d
milcab <milcarodriguez23@gmail.com>
2021-12-22 14:27:26 -0500

    adding places view with mock data

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,36 +1,19 @@
 const React = require("react");
 const Default = require("../layouts/default");
 
-const Items = ({ items }) => {
-  return (
-    <div className="container">
-      <div className="row">
-        {items.map((items, index) => {
-          return (
-            <div key={index} className="col-md-4">
-              <div className="card card mb-4 shadow-sm">
-                <img src={items.pic} className="card-img-top" />
-                <div className="card-body">
-                  <h5 className="card-title">{items.name}</h5>
-                  <p className="card-text">{items.city}</p>
-                  <p className="card-text">{items.state}</p>
-                  <p className="card-text">{items.cuisines}</p>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+function Index({ places }) {
+  const placesFormatted = places.map((place) => (
+    <div>
+      <h2>{place.name}</h2>
+      <img src={place.pic} alt={place.name} />
     </div>
-  );
-};
+  ));
 
-function Index({ title = "places", places = [] }) {
   return (
-    <Default title={title}>
+    <Default>
       <h1>Places Page</h1>
 
-      <Items items={places} />
+      {placesFormatted}
     </Default>
   );
 }
