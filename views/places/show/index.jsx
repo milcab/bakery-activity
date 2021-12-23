ab14f5e663b58afc4949e6d1a24d398308f67a8e
milcab <milcarodriguez23@gmail.com>
2021-12-23 18:25:03 -0500

    Jazzed up form page

--- views/places/show/index.jsx
+++ views/places/show/index.jsx
@@ -1,14 +1,14 @@
 const React = require("react");
+const Default = require("../../layouts/default");
 const Def = require("../../layouts/default");
 // views/layouts/default.jsx
-function Index() {
-  return (
-    <Def>
-      <main>
-        <h1>Show</h1>
-      </main>
-    </Def>
-  );
+function show({bread}) {
+  console.log(bread.name)
+    return(
+      <Default>
+        <h2>Show Page</h2>
+      </Default>
+    )
 }
 
-module.exports = Index;
+module.exports = show;
