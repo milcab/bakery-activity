0d9e47cef75bd1c8400bbd557e6446eec18993ee
milcab <milcarodriguez23@gmail.com>
2022-01-01 00:22:58 -0500

    i added new place.js

--- controllers/places.js
+++ controllers/places.js
@@ -1,18 +1,7 @@
 const express = require('express')
 const router = express.Router()
-let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'http://placekitten.com/250/250'
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'http://placekitten.com/250/250'
-}]
+
+const places = require('../models/places')
 
 router.get('/', (req, res) => {
     res.render('places/index', { places })
@@ -23,7 +12,18 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/new', (req, res) => {
-    places.push(req.body);
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+
+    places.push(req.body)
     res.redirect('/places')
 })
 
