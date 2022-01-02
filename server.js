4d896054970ca201ffa97a9196767875892b97a3
milcab <milcarodriguez23@gmail.com>
2022-01-01 21:03:10 -0500

    remove bakaery code

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
@@ -14,8 +15,6 @@ app.engine('jsx', require('express-react-views').createEngine())
 
 
 app.use('/breads', require('./controllers/breads_controller.js'))
-
-app.use('/places', require('./controllers/places.js'))
 
 app.get('*', (req, res) => {
     res.render('404/index')
