8db7191d97e8ee8151760246bfe174304f9edada
milcab <milcarodriguez23@gmail.com>
2022-01-12 16:30:36 -0400

    fixing the route with the model

--- controllers/breads.js
+++ controllers/breads.js
@@ -1,20 +1,25 @@
 const express = require('express')
 const breadsRouter = express.Router()
-const breads = require('../models/bread')
+
+const bread = require('../models/bread')
 
 // show list of breads
-breadsRouter.get('/', (req, res) => {
+breadsRouter.get('/', async (req, res) => {
+    const breads = await bread.find()
+
     res.render('breads/list', {
         title: "breads page",
-        breads: breads
+        breads
     })
 })
 
 // SHOW bread by index
-breadsRouter.get('/:arrayIndex', (req, res) => {
-    if (breads[req.params.arrayIndex]) {
+breadsRouter.get('/show/:breadId', async (req, res) => {
+    const foundBread = await bread.findById(req.params.breadId);
+
+    if (foundBread) {
         res.render('breads/show', {
-            bread: breads[req.params.arrayIndex],
+            bread: foundBread,
             index: req.params.arrayIndex,
         })
     } else {
@@ -23,7 +28,9 @@ breadsRouter.get('/:arrayIndex', (req, res) => {
 })
 
 // SHOW bread by index
-breadsRouter.get('/:arrayIndex/edit', (req, res) => {
+breadsRouter.get('/:arrayIndex/edit', async (req, res) => {
+    const breads = await bread.find();
+
     if (breads[req.params.arrayIndex]) {
         res.render('breads/edit', {
             bread: breads[req.params.arrayIndex],
@@ -40,7 +47,8 @@ breadsRouter.get('/new', (req, res) => {
 })
 
 // SHOW edit bread form
-breadsRouter.get('/edit/:arrayIndex', (req, res) => {
+breadsRouter.get('/edit/:arrayIndex', async (req, res) => {
+    const breads = await bread.find();
     if (breads[req.params.arrayIndex]) {
         res.render('breads/edit', {
             bread: breads[req.params.arrayIndex],
@@ -53,33 +61,43 @@ breadsRouter.get('/edit/:arrayIndex', (req, res) => {
 
 // UPDATE
 breadsRouter.put('/:arrayIndex', (req, res) => {
+    if (!req.body.image) {
+        req.body.image = undefined
+    }
+
     if (req.body.hasGluten === 'on') {
         req.body.hasGluten = true
     } else {
         req.body.hasGluten = false
     }
 
-    breads[req.params.arrayIndex] = req.body
+    // breads[req.params.arrayIndex] = req.body
     res.redirect(`/breads`)
 })
 
 // NEW
 breadsRouter.post('/:arrayIndex', (req, res) => {
-    // TODO: add new bread to the breads array
+    req.body.image = req.body.image || undefined
+    req.body.hasGluten = req.body.hasGluten === 'on'
+
+    bread.create(req.body).then(() => {
+        res.redirect('/breads')
+    });
 })
 
 breadsRouter.delete('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!breads[id]) {
-        res.render('error404')
-    }
-    else {
-        breads.splice(id, 1)
-        res.redirect('/breads')
-    }
+    // const breads = await bread.find();
+    // let id = Number(req.params.id)
+    // if (isNaN(id)) {
+    //     res.render('error404')
+    // }
+    // else if (!breads[id]) {
+    //     res.render('error404')
+    // }
+    // else {
+    //     // bread.splice(id, 1)
+    res.redirect('/breads')
+    // }
 })
 
 module.exports = breadsRouter
