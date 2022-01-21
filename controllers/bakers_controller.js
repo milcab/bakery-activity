58fd218048f6be40bf76a88ad4ea73d030cb02a3
milcab <milcarodriguez23@gmail.com>
2022-01-21 17:23:19 -0500

    adding index route

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -3,6 +3,23 @@ const express = require('express')
 const baker = express.Router()
 const Baker = require('../models/baker.js')
 const bakerSeedData = require('../models/baker_seed.js')
+// in the new route
+baker.get('/new', (req, res) => {
+    Baker.find()
+        .then(foundBakers => {
+            res.render('breads/new', {
+                bakers: foundBakers
+            })
+        })
+})
+
+// Index: 
+baker.get('/', (req, res) => {
+    Baker.find()
+        .then(foundBakers => {
+            res.send(foundBakers)
+        })
+})                    
 
 baker.get('/data/seed', (req, res) => {
     Baker.insertMany(bakerSeedData)
@@ -10,5 +27,5 @@ baker.get('/data/seed', (req, res) => {
 })
 
 // export
-module.exports = baker 
+module.exports = baker
 
